869919d83d21d9396a3bdb6b6ef853aae084b149
igorszwedzinski <igor.szwedzinski@gmail.com>
2021-04-16 14:22:11 +0200

    updae pracy domowej

--- lesson10/pracadomowa.js
+++ lesson10/pracadomowa.js
@@ -7,9 +7,9 @@ console.log(addThreeNumbers(1, 2, 3))
 //Zadanie 1b
 
 function triangleSurface(height, width) {
-    console.log(`Pole trójkąta to ${width * height / 2} czyli ma on ${width} długości podstawy i ${height} wysokości`)
+    return `Pole trójkąta to ${width * height / 2} czyli ma on ${width} długości podstawy i ${height} wysokości`
 }
-triangleSurface(1, 2)
+console.log(triangleSurface(1, 2))
 
 //Zadanie 1c
 
@@ -29,9 +29,9 @@ console.log(fuelConsumption(10, 100))
 //Zadanie 1e
 
 function sentence(name, age, kids) {
-    console.log(`${name} ma ${age} lat i ${kids} dzieci`)
+    return `${name} ma ${age} lat i ${kids} dzieci`
 }
-sentence ("Ala",23,2)
+console.log(sentence("Ala",23,2))
 
 //Zadanie 2a
 
@@ -53,15 +53,15 @@ console.log(return2ndElement(table2))
 
 let table3 = [1, 2, 3]
 function returnLength(inputTable) {
-    console.log(`Przekazana tablica ma ${inputTable.length} elementów, a elementem nr 3 jest ${inputTable[2]}`)
+    return `Przekazana tablica ma ${inputTable.length} elementów, a elementem nr 3 jest ${inputTable[2]}`
 }
-returnLength(table3)
+console.log(returnLength(table3))
 
 //Zadanie 2d
 
 let table4 = []
 function returnInjectedTable(inputTable, value) {
-    inputTable[0] = value
+    inputTable.push(value)
     return inputTable
 }
 console.log(returnInjectedTable(table4, "Ala"))
@@ -83,9 +83,9 @@ console.log(returnName(person, "Ala"))
 //Zadanie 2f
 
 function returnPerson(firstName, lastName, age, height) {
-    console.log(`${firstName} ${lastName} ma ${age} lat i ${height} cm wzrostu`)
+    return `${firstName} ${lastName} ma ${age} lat i ${height} cm wzrostu`
 }
-returnPerson("Igor", "Szwedziński", 49, 193)
+console.log(returnPerson("Igor", "Szwedziński", 49, 193))
 
 //Zadanie 2g
 
@@ -102,68 +102,68 @@ console.log(returnChangedPerson(person2, 25))
 
 function allowedToDrive(age, hasDrivingLicence) {
     if (age >= 18 && hasDrivingLicence == true){
-        console.log("Mozesz śmiało jeździć")
+        return "Mozesz śmiało jeździć"
     } else if (age >=18 && hasDrivingLicence==false) {
-        console.log("Nie ma prawa jazdy")
+        return "Nie ma prawa jazdy"
     } else {
-        console.log("Nie ma prawa jazdy")
+        return "Nie ma prawa jazdy"
     }
 }
-allowedToDrive(18,true)
-allowedToDrive(18, false)
-allowedToDrive(17, true)
-allowedToDrive(17, false)
+console.log(allowedToDrive(18,true))
+console.log(allowedToDrive(18, false))
+console.log(allowedToDrive(17, true))
+console.log(allowedToDrive(17, false))
 
 //Zadanie 3b
 
 function sellBuy(action) {
     if (action == "Kup") {
-        console.log('Widzę, że zrobisz zakupy')
+        return 'Widzę, że zrobisz zakupy'
     } else if (action == "Sprzedaj") {
-        console.log('Mozesz tutaj swobodnie sprzedawać')
+        return 'Mozesz tutaj swobodnie sprzedawać'
     } else {
-        console.log('Nie wybrałeś żadnej z dostępnych opcji - wpisz Kup lub Sprzedaj')
+        return 'Nie wybrałeś żadnej z dostępnych opcji - wpisz Kup lub Sprzedaj'
     }
 }
 
-sellBuy("Kup")
-sellBuy("Sprzedaj")
-sellBuy("Nie wiem co mam robić")
+console.log(sellBuy("Kup"))
+console.log(sellBuy("Sprzedaj"))
+console.log(sellBuy("Nie wiem co mam robić"))
 
 //Zadanie 3c
 
 function positiveNegativeZero(number) {
     if (number > 0) {
-        console.log("Liczna jest dodatnia")
+        return "Liczna jest dodatnia"
     } else if (number < 0) {
-        console.log("Liczna jest ujemna")
+        return "Liczna jest ujemna"
     } else {
-        console.log("Liczna jest równa 0")
+        return "Liczna jest równa 0"
     }
 }
 
-positiveNegativeZero(5)
-positiveNegativeZero(-6)
-positiveNegativeZero(0)
+console.log(positiveNegativeZero(5))
+console.log(positiveNegativeZero(-6))
+console.log(positiveNegativeZero(0))
 
 //Zadanie 3d
 
 function modulo(number) {
     if (number % 3 == 0) {
-        console.log("Fizz")
+        return "Fizz"
     } else if (number % 5== 0) {
-        console.log("Buzz")
+        return "Buzz"
     } else {
-        console.log("FizzBuzz")
+        return "FizzBuzz"
     }
 }
 
-modulo(3)
-modulo(21)
-modulo(5)
-modulo(100)
-modulo(7)
-modulo(8)
+console.log(modulo(3))
+console.log(modulo(21))
+console.log(modulo(5))
+console.log(modulo(100))
+console.log(modulo(7))
+console.log(modulo(8))
 
 //Zadanie 3e
 
@@ -211,32 +211,35 @@ let car = {
     model: "Astra",
     marka: "Opel",
     rokProdukcji: 1999,
-    isCabrio: false,
+    isCabrio: true,
 }
 function whatCarIsIt(inputCar) {
     var date = new Date();
     
     if (inputCar.rokProdukcji > date.getFullYear()) {
-        console.log("Tego auta jeszcze nie wyprodukowano")
-    } else {
-        (inputCar.isCabrio) ? console.log(`Typ nadwozia: Kabriolet, Model: ${inputCar.model}, Marka: ${inputCar.marka}, Rok produkcji: ${inputCar.rokProdukcji}`) : console.log(`Typ nadwozia: Niekabriolet, Model: ${inputCar.model}, Marka: ${inputCar.marka}, Rok produkcji: ${inputCar.rokProdukcji}`)
+        return "Tego auta jeszcze nie wyprodukowano"
     }
+    if (inputCar.isCabrio) {
+        return `Typ nadwozia: Kabriolet, Model: ${inputCar.model}, Marka: ${inputCar.marka}, Rok produkcji: ${inputCar.rokProdukcji}` 
+    }
+
+    return `Typ nadwozia: Niekabriolet, Model: ${inputCar.model}, Marka: ${inputCar.marka}, Rok produkcji: ${inputCar.rokProdukcji}`
 }
 
-whatCarIsIt(car)
+console.log(whatCarIsIt(car))
 
 //Zadanie 4b
 
-let table10 = []
+let table10 = [1,2,3]
 function tableLength(inputTable) {
     if (inputTable.length == 0) {
-        console.log("Tablica jest pusta")
+        return "Tablica jest pusta"
     } else {
-        console.log(`Tablica ma ${inputTable.length} elementów`)
+        return `Tablica ma ${inputTable.length} elementów`
     }
 }
 
-tableLength(table10)
+console.log(tableLength(table10))
 
 //Zadanie 4c
 
@@ -245,7 +248,7 @@ let osoba = {
     nazwisko: "Jaskóła",
     wiek: 23,
     czyLubiCzytacKsiazki: true,
-    maTV: true,
+    maTV: false,
 }
 
 let ksiazka = {
@@ -256,10 +259,14 @@ let ksiazka = {
 
 function recommendation(inputOsoba, inputKsiazka) {
     if (inputOsoba.czyLubiCzytacKsiazki == true) {
-        console.log(`Cześć ${inputOsoba.imie}, polecam Ci przeczytać ${inputKsiazka.tytul}, ma tylko ${inputKsiazka.liczbaStron} stron.`)
-    } else {
-        (inputOsoba.maTV) ? console.log("Idź oglądać TV") : console.log("Nie wiem co Ci zaproponować")
+        return `Cześć ${inputOsoba.imie}, polecam Ci przeczytać ${inputKsiazka.tytul}, ma tylko ${inputKsiazka.liczbaStron} stron.`
     }
+    
+    if (inputOsoba.maTV) {
+        return "Idź oglądać TV" 
+    }
+    
+    return "Nie wiem co Ci zaproponować"
 }
 
-recommendation(osoba,ksiazka)
+console.log(recommendation(osoba,ksiazka))
